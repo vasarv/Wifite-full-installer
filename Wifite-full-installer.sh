6a5eabc57d64e3bfa09e78b288e088bff9e19c21
vasarv <76836836+vasarv@users.noreply.github.com>
2022-03-08 18:04:11 +0300

    Update Wifite-full-installer.sh

--- Wifite-full-installer.sh
+++ Wifite-full-installer.sh
@@ -3,10 +3,10 @@ sudo chmod +x install-Atheros-AR9271-wifi-driver.sh &&
 sudo chmod +x Pyrit-installer.sh &&
 sudo chmod +x wifite-installer.sh &&
 echo "Execution rights granted to files:
-Hcxpcaptool-installer.sh
-install-Atheros-AR9271-wifi-driver.sh
-Pyrit-installer.sh
-wifite-installer.sh" &&
+sudo Hcxpcaptool-installer.sh &&
+sudo /usr/bin/python2 -m pip install --upgrade pip
+sudo Pyrit-installer.sh &&
+sudo wifite-installer.sh" &&
 sudo ./Hcxpcaptool-installer.sh &&
 sudo ./Pyrit-installer.sh &&
 sudo ./wifite-installer.sh &&
